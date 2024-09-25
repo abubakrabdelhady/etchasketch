23e3df126dd3f756ae6b8401f7a95838caae77ce
Abubakr Abdelhady <171250585+abubakrabdelhady@users.noreply.github.com>
2024-09-25 18:33:31 +0300

    Color on hover, change size and clear grid

--- js/main.js
+++ js/main.js
@@ -1,24 +1,49 @@
 // Just Start
 console.log('Start Etch-a-Sketch!');
 
+const gridWidth = 600;
+const defaultCellsInRow = 16;
+
 // grid div
-let grid = document.querySelector('.grid');
+const grid = document.querySelector('.grid');
+grid.style.width = gridWidth;
 
-// Grid and cell widths
-let widthUnit = 'rem';
-// one cell width
-let cellWidth = 2;
-// number of cells in row
-let cellsInRow = 16;
+const changeSizeButton = document.querySelector('.change-size');
+changeSizeButton.addEventListener('click', function() {
+    let cellsInRow = prompt('How many pixels (squares) you want in every row? (max:100)');
+    if(cellsInRow > 100) alert('Please enter maximum 100!');
+    else createGrid(cellsInRow);
+})
 
-// calculate and adjust grid width
-let gridWidth = cellWidth * cellsInRow;
-grid.style.width = `${gridWidth}${widthUnit}`;
+const clearButton = document.querySelector('.clear-grid');
+clearButton.addEventListener('click', function() {
+    let confirmClear = confirm('Are you sure you want to clear the grid?');
+    if(confirmClear) {
+        let cells = document.querySelectorAll('.cell-visited');
+        
+        cells.forEach(function(cell) {
+            cell.classList.remove('cell-visited');
+        })
+    }
+})
 
-// Create grid cells
-for(let i = 1; i <= (cellsInRow*cellsInRow); i++) {
-    let cell = document.createElement('div');
-    cell.classList.add('cell', `cell-${i}`);
-    cell.style.width = cell.style.height = `${cellWidth}${widthUnit}`;
-    grid.appendChild(cell);
+
+createGrid(defaultCellsInRow);
+
+function createGrid(cellsInRow) {
+    // empty grid
+    grid.innerHTML = "";
+    // one cell width
+    let cellWidth = gridWidth/cellsInRow;
+
+    // Create grid cells
+    for(let i = 1; i <= (cellsInRow*cellsInRow); i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell', `cell-${i}`);
+        cell.style.width = cell.style.height = `${cellWidth}px`;
+        cell.addEventListener('mouseenter', function() {
+            this.classList.add('cell-visited');
+        })
+        grid.appendChild(cell);
+    }
 }
